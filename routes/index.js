ded10076e4cea371a0c9d88e8bd251ff079bd58a
Jose Alberto Rodríguez <josuku@MacBook-Pro-de-Jose.local>
2015-08-08 18:31:11 +0200

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
-/* GET home page. */
+// Pagina de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
@@ -12,6 +13,12 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
 
+// Definicion de rutas de sesion
+router.get('/login', 						sessionController.new);
+router.post('/login', 						sessionController.create);
+router.get('/logout', 						sessionController.destroy);
+
+// Definicion de rutas de quizes
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
@@ -21,6 +28,7 @@ router.get('/quizes/:quizId(\\d+)/edit',	quizController.edit);
 router.put('/quizes/:quizId(\\d+)',			quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
 
+// Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
 
