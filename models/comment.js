476065d0e6520c0f9c2a5b97c69c395b77a77d76
Jose Alberto Rodríguez <josuku@gmail.com>
2015-08-08 23:26:20 +0200

    Moderación de comentarios

--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,16 @@
 // Definicion del modelo de Quiz
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('Comment',
-		{ 	texto: {
+	return sequelize.define(
+		'Comment',
+		{ 	
+			texto: {
 				type: DataTypes.STRING,
 				validate: { notEmpty: {msg: "-> Falta Comentario"} }
+			},
+			publicado: {
+				type: DataTypes.BOOLEAN,
+				defaultValue: false
 			}
 	  	});
 }
